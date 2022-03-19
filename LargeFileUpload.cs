0f1e254de2eff7cd64f56c36157da115a96d9dac
PatrickJones <hiramdeveloper@gmail.com>
2022-03-19 09:25:56 -0700

    Corrected typo and removed comments

--- LargeFileUpload.cs
+++ LargeFileUpload.cs
@@ -13,12 +13,14 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net;
+using System.Text;
 
 namespace ContentHubLargeFileUpload
 {
     public static class LargeFileUpload
     {
         const string _mimeType = "application/json";
+        const string TOKEN_HEADER = "X-Auth-Token";
 
         [FunctionName("LargeFileUpload")]
         public static async Task<IActionResult> Run(
@@ -43,57 +45,57 @@ namespace ContentHubLargeFileUpload
                 //string val = fileData.ToObject<string>();
                 client.BaseAddress = new Uri(data.ContentHubHostName);
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_mimeType));
-                client.DefaultRequestHeaders.Add("X-Auth-Toklen", data.ContentHubToken);
+                client.DefaultRequestHeaders.Add(TOKEN_HEADER, data.ContentHubToken);
 
                 var base64EncodedBytes = System.Convert.FromBase64String(fileData);
                 var result = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
 
                 int chunkCounter = 0;
                 Dictionary<int, byte[]> fileChunks = new Dictionary<int, byte[]>();
-                foreach (var chunk in base64EncodedBytes.ToChunks(1000))
+                foreach (var chunk in base64EncodedBytes.ToChunks(2 * 1024))
                 {
                     fileChunks.Add(chunkCounter, chunk.ToArray()); ;
                     chunkCounter++;
                 }
 
                 var upload = await GetUploadUrl(data.ContentHubHostName, data.Filename, data.FileSize, data.ContentHubToken);
+                
                 foreach (var fileChunk in fileChunks.OrderBy(o => o.Key))
                 {
-                    //var uploadUrl = fileChunk.Key == 0 
-                    //    ? $"{data.ContentHubUploadUrl}&chunks={fileChunks.Count}" 
-                    //    : $"{data.ContentHubUploadUrl}&chunks={fileChunks.Count}&chunk={fileChunk.Key}";
-
-                    var uploadUrl = fileChunk.Key == 0
-                        ? $"{upload.Key}&chunks={fileChunks.Count}"
-                        : $"{upload.Key}&chunks={fileChunks.Count}&chunk={fileChunk.Key}";
-
-
-                    var resp = await client.PostAsync(uploadUrl, new ByteArrayContent(fileChunk.Value));
-
-                    if (!resp.IsSuccessStatusCode)
+                    using (var ms = new MemoryStream())
                     {
-                        return new BadRequestObjectResult(await resp.Content.ReadAsStringAsync());
+                        ms.Write(fileChunk.Value);
 
+                        //var uploadUrl = $"{data.ContentHubUploadUrl}&chunks={fileChunks.Count}&chunk={fileChunk.Key}";
+                        var uploadUrl = $"{upload.Key}&chunks={fileChunks.Count}&chunk={fileChunk.Key}";
+
+                        //var resp = await client.PostAsync(uploadUrl, new StreamContent(ms));
+
+                        var multipartContent = new MultipartFormDataContent();
+                        var byteArrayContent = new ByteArrayContent(fileChunk.Value);
+                        byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(data.MediaType);
+                        byteArrayContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                        {
+                            FileName = data.Filename,
+                            Name = "file",
+                        };
+                        multipartContent.Add(byteArrayContent);
+                        var resp = await client.PostAsync(uploadUrl, multipartContent);
+                        if (!resp.IsSuccessStatusCode)
+                        {
+                            return new BadRequestObjectResult(await resp.Content.ReadAsStringAsync());
+
+                        }
                     }
                 }
 
-                await FinalizeUpload(data.ContentHubHostName, upload.Value, data.ContentHubToken);
-                // log.Info($"file data val : "+result);
-                //StringReader stringReader = new StringReader(result);
-                //rest you can do yourself
+                await client.PostAsync($"{upload.Key}&chunks={fileChunks.Count}", null);
+                var finalResp = await FinalizeUpload(data.ContentHubHostName, upload.Value, data.ContentHubToken);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
             }
-
-            //log.LogInformation("C# HTTP trigger function processed a request.");
-
-            //string name = req.Query["name"];
-
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //name = name ?? data?.name;
 
             string responseMessage = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
             return new OkObjectResult(responseMessage);
@@ -104,22 +106,26 @@ namespace ContentHubLargeFileUpload
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(hostUrl);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_mimeType));
-            client.DefaultRequestHeaders.Add("Content-Type", _mimeType);
-            client.DefaultRequestHeaders.Add("X-Auth-Toklen", token);
+            client.DefaultRequestHeaders.Add(TOKEN_HEADER, token);
 
-
-            var payload = new { 
+            var payload = new {
+                action = new { 
+                    name = "NewAsset", 
+                    parameters = new { }
+                },
                 file_name = fileName, 
-                file_size = fileSize, 
+                file_size = fileSize.ToString(), 
                 upload_configuration = new { 
                     name = "ApprovedAssetUploadConfiguration", 
                     parameters = new { }
                 }
             };
 
-            var resp = await client.PostAsync("api/v2.0/upload", new StringContent(JsonConvert.SerializeObject(payload)));
+            var jsonPayload = JsonConvert.SerializeObject(payload);
+
+            var resp = await client.PostAsync("api/v2.0/upload", new StringContent(jsonPayload, Encoding.UTF8, _mimeType));
             return resp.IsSuccessStatusCode 
-                ? new KeyValuePair<string, HttpContent>(resp.Headers.GetValues("location").FirstOrDefault(), resp.Content) 
+                ? new KeyValuePair<string, HttpContent>(resp.Headers.GetValues("Location").FirstOrDefault(), resp.Content) 
                 : new KeyValuePair<string, HttpContent>(String.Empty, null);
         }
 
@@ -128,10 +134,10 @@ namespace ContentHubLargeFileUpload
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(hostUrl);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_mimeType));
-            client.DefaultRequestHeaders.Add("Content-Type", _mimeType);
-            client.DefaultRequestHeaders.Add("X-Auth-Toklen", token);
+            client.DefaultRequestHeaders.Add(TOKEN_HEADER, token);
 
-            var resp = await client.PostAsync("api/v2.0/upload/Finalize", httpContent);
+            var resp = await client.PostAsync("api/v2.0/upload/finalize", httpContent);
+            var json = await resp.Content.ReadAsStringAsync();
             return (int)resp.StatusCode;
         }
     }
